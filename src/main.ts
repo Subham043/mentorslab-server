40c969194a9db541f4e3dd3436d465fdd86a1b50
Subham Saha <subham@Subhams-MacBook-Pro.local>
2022-09-27 01:09:42 +0530

    fixed form error format

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { ValidationPipe } from '@nestjs/common';
+import { HttpException, HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { useContainer } from 'class-validator';
+import { useContainer, ValidationError } from 'class-validator';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
@@ -14,20 +14,32 @@ async function bootstrap() {
   app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        const result = {};
+        validationErrors.forEach((element) => {
+          console.log(element);
+          result[element.property] = Object.values(element.constraints);
+        });
+
+        return new HttpException(
+          { form_error: result },
+          HttpStatus.BAD_REQUEST,
+        );
+      },
       whitelist: true,
     }),
   );
   app.enableCors({
     origin: function (origin, callback) {
-      // const whitelist = ['http://127.0.0.1:3000', 'http://localhost:3000'];
-      // if (origin) {
-      //   if (whitelist.find((x) => origin.endsWith(x))) {
-      //     callback(null, true);
-      //   } else {
-      //     callback(new Error('Not allowed by CORS'));
-      //   }
-      // } else callback(null, true);
-      callback(null, true);
+      const whitelist = ['http://127.0.0.1:3000', 'http://localhost:3000'];
+      if (origin) {
+        if (whitelist.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      } else callback(null, true);
+      // callback(null, true);
     },
   });
   await app.listen(process.env.PORT || 3300);

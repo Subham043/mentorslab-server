3b140359e25e8411cf58d0801e2426df677cefdd
Subham Saha <subham@Subhams-MacBook-Pro.local>
2022-10-18 00:59:05 +0530

    added new tables and columns to existing table

--- prisma/migrations/20220928184424_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `hased_refresh_token` VARCHAR(191) NULL;
--- prisma/migrations/20220928192205_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `hased_refresh_token` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `User` DROP COLUMN `hased_refresh_token`,
-    ADD COLUMN `hashed_refresh_token` VARCHAR(191) NULL;
